fa7d373027c2fdad4044ce3e6b76172fa821edb4
Richard Mazel <richardmmy28@gmail.com>
2019-10-28 18:35:01 -0700

    Added JSON File

--- apiRoutes.js
+++ apiRoutes.js
@@ -25,10 +25,9 @@ module.exports = function (app) {
           newFriend.scores[i] = parseInt(newFriend.scores[i]);
         }
       }
-      // compare the scores of newFriend with the scores of each friend in the database and find the friend with the smallest difference when each set of scores is compared
-
+      
       var bestMatchIndex = 0;
-      //greatest score difference for a question is 4, therefore greatest difference is 4 times # of questions in survey
+      
       var bestMatchDifference = 40;
 
       for(var i = 0; i < friends.length; i++) {
@@ -39,21 +38,21 @@ module.exports = function (app) {
           totalDifference += differenceOneScore;
         }
 
-        // if the totalDifference in scores is less than the best match so far
-        // save that index and difference
+        
+        
         if (totalDifference < bestMatchDifference) {
           bestMatchIndex = i;
           bestMatchDifference = totalDifference;
         }
       }
 
-      // the best match index is used to get the best match data from the friends index
+      
       bestMatch = friends[bestMatchIndex];
 
-      // Put new friend from survey in "database" array
+      
       friends.push(newFriend);
 
-      // return the best match friend
+      
       res.json(bestMatch);
   });
 
